19adeab757e91d529dd7c4417c4b55ea56d5cbf8
nicholas <ncl1984@gmail.com>
2019-08-21 04:30:56 +0000

    student enrollment problems 2

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,6 +3,7 @@ class LessonsController < ApplicationController
   before_action :require_authorized_for_current_lesson, only: [:show]
 
   def require_authorized_for_current_lesson
+    redirect_to course_path(current_user), alert: 'Not Valid!'
   end
 
 
