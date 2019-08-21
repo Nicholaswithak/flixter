f49e56bd85c615269e5b7e44d5d0d5245692a6d4
nicholas <ncl1984@gmail.com>
2019-08-21 03:43:46 +0000

    student enrollment problems

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,9 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_lesson.section.course, only: [:show]
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
-  redirect_to root_url, alert: 'Error Message Here'
+  def require_authorized_for_current_lesson
+  end
 
 
   def show
@@ -11,6 +12,7 @@ class LessonsController < ApplicationController
   private
 
   helper_method :current_lesson
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
