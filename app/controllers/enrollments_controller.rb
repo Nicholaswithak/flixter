01b4fc8b2bf5d97300dc8fb9010ed8e16ec9b267
nicholas <ncl1984@gmail.com>
2019-08-22 22:14:11 +0000

    trying to get stripe to work

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,14 +1,33 @@
   class EnrollmentsController < ApplicationController
   before_action :authenticate_user!
 
-  def create
-    current_user.enrollments.create(course: current_course)
-    redirect_to course_path(current_course)
-  end
+    def create
+      # Amount in cents
+      @amount = 500
 
-  private
+      customer = Stripe::Customer.create({
+        email: params[:stripeEmail],
+        source: params[:stripeToken],
+      })
 
-  def current_course
-    @current_course ||= Course.find(params[:course_id])
-  end
+      charge = Stripe::Charge.create({
+        customer: customer.id,
+        amount: @amount,
+        description: 'Rails Stripe customer',
+        currency: 'usd',
+      })
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    end
+      current_user.enrollments.create(course: current_course)
+      redirect_to course_path(current_course)
+    end
+
+    private
+
+    def current_course
+      @current_course ||= Course.find(params[:course_id])
+    end
 end
