183b91d134039bb313d5014a9d939bad385ecc52
nicholas <ncl1984@gmail.com>
2019-08-30 17:46:04 +0000

    lesson 32 complete

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_section, only: [:update]
 
 
   def new
@@ -22,11 +23,15 @@ class Instructor::SectionsController < ApplicationController
 
   helper_method :current_course
   def current_course
+    if params[:course_id]
     @current_course ||= Course.find(params[:course_id])
+  else
+    current_section.course
+  end
   end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 
 end
