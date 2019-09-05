1cdbbe173d5906484e2c8ea9a4388b6db94481ab
nicholas <ncl1984@gmail.com>
2019-09-05 22:37:28 +0000

    working on finishing app

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -30,9 +30,9 @@ class VideoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :medium do
-    process resize_to_fit: [192, 135]
-  end
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
